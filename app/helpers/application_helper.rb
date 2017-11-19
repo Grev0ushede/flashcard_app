8d823d006d199b1518ad2783faa8dab62daf9149
Grev0ushede <w00taliter@gmail.com>
2017-11-19 11:18:27 -0800

    Added more custom helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Returns full title based on pages
+  def full_title(page_title = '')
+    base_title = "Flashcard"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,26 +3,27 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = " | Flashcard"
+    @base_title = "Flashcard"
+    @mixed_title = "| #{@base_title}"
   end
 
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home#{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help#{@base_title}"
+    assert_select "title", "Help #{@mixed_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About#{@base_title}"
+    assert_select "title", "About #{@mixed_title}"
   end
 
 end
